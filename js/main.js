d346ab0ced49740bd255f0ca133ece34ba188fd8
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-03-16 17:12:45 -0600

    Falta la parte de subirlo a un host

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 const menuBtn = document.querySelector('#menu-btn');
 const ulLeft = document.querySelector('#ul-left')
 
+const showCase = document.querySelector('.show-case')
+
 menuBtn.addEventListener('click',()=>{ 
     ulLeft.classList.toggle('show');
 
 });
+
+ScrollReveal().reveal(showCase);
+ScrollReveal().reveal('.news-cards',{delay:500});
+ScrollReveal().reveal('.cards-banner-one',{delay:500});
+ScrollReveal().reveal('.cards-banner-two',{delay:500});
